5da3c2759d9317dc8c7c11455e62f0c73fb41135
Oleg A. Khlybov <fougas@mail.ru>
2022-06-16 15:42:47 +0500

    Tests fixes.

--- basic_spec.sh
+++ basic_spec.sh
@@ -6,16 +6,24 @@ Context "basic"
   Parameters:matrix
     -static -
   End
-  Example "C $1 test"
+  Example "C $1 test" c
     When call test_program hello.c
+    The output should equal "hello"
     The status should be success
   End
-  Example "C++ $1 test"
+  Example "C source $1 test with C++ compiler" c++
+    When call test_program -x c++ hello.c
+    The output should equal "hello"
+    The status should be success
+  End
+  Example "C++ $1 test" c++
     When call test_program hello.cpp
+    The output should equal "hello"
     The status should be success
   End
-  Example "FORTRAN $1 test"
+  Example "FORTRAN $1 test" fortran
     When call test_program hello.f90
+    The output should equal " hello"
     The status should be success
   End
 End
--- mpi_spec.sh
+++ mpi_spec.sh
@@ -2,20 +2,23 @@
 
 eval "$(shellspec - -c) exit 1"
 
-Context 'MPI'
+Context "MPI"
   Parameters:matrix
     - -static
   End
   Example "C test $1" mpi c
     When call test_program --mpi $1 hello_mpi.c
+    The output should match pattern "*"
     The status should be success
   End
   Example "C++ test $1" mpi c++
     When call test_program --mpi $1 hello_mpi.cpp
+    The output should match pattern "*"
     The status should be success
   End
   Example "FORTRAN test $1" mpi fortran
     When call test_program --mpi $1 hello_mpi.f90
+    The output should match pattern "*"
     The status should be success
   End
 End
